2a1df6c2712693faa37ab4cf90df5bf5a3d276de
Richard Kelly <rkelly@sitscape.com>
2012-04-13 23:19:51 -0400

    Basic class structure and IA

--- punchlist.rb
+++ punchlist.rb
@@ -1,3 +1,18 @@
-def punchlist
+class Punchy
+	# Punchy runs the command line stuff, triggers projects and tasks classes
+	#   Project		Gungho-Guides folder containing 
+	#     |_Feature		features.md headings are features, paragraphs under are descriptions of tasks
+	#     | |_Tasks		Ordered list of tasks pertaining to each feature
+	#     |
+	#     |_Bugs		bugs.md headings are bugs, paragraphs under are descriptions of tasks
+	#       |_tasks		Ordered list of tasks pertaining to each bug
+
+	# Creates basic method and instance vars
+	def initialize
 	
+	end
+
+	def home_screen
+
+	end
 end
