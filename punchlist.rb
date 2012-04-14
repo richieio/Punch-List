b94fb0d31f9fc9154931d89fae4750f54eba969a
Zach McGonigal <zach.mcgonigal@gmail.com>
2012-04-14 17:19:37 -0400

    add some comments. make some code be functions to be used later on when we write the features and bugs class that extends from this

--- punchlist.rb
+++ punchlist.rb
@@ -8,36 +8,49 @@ class Punchy
 	#       |_tasks		Ordered list of tasks pertaining to each bug
 
 	# Creates basic method and instance vars
-	attr_accessor :command, :numDirectories
+	attr_accessor :command, :numDirectories, :currentDirectory
 
 	def initialize
 		@command = ""	
 		@directories = Array.new 
+		@currentDirectory = Dir.getwd
 	end
 
 	# Lists all the directories in the current path not starting with a "."
 	def directory_listing
-		Dir.foreach('.') do |f|
-			if f[0] != "."
-				if File.directory?(f)
-					@directories[@directories.size] = f
-					puts "[#{@directories.size}] - #{f}"
+		Dir.glob('*') do |f|#go through all the files in the directory that don't start with a "."
+				if File.directory?(f)#if file is a directory, then it's a project
+					@directories[@directories.size] = f#add the file to the directory arrray
+					puts "[#{@directories.size}] - #{f}"#display the directory and the number associated to it
 				end
-			end
 		end
 	end
 
-	def home_screen
-		while @command != ":q"
-			self.directory_listing
+	#the menu to be looped through
+	def display_menu
+		puts ""
+		puts "You are currently in #{@currentDirectory}"
+		puts "Enter the number next to the project file to open that project, :q to quit"
+		puts ""
+	end
 
-			puts "Enter command : (:q to quit)"
-			@command = gets.to_s.strip
-			commandI = @command.to_i
+	#handle what the user inputed
+	def input
+		@command = gets.to_s.strip #must strip to get rid of \n when user hits enter
+		commandI = @command.to_i #convert input to integer to see what number they pressed
+		
+		if commandI <= (@directories.size) && commandI > 0 #many strings will convert to integer 0, therefore we cannot use 0 as a number to be used
+			puts "You selected #{@directories[commandI]}"
+		end
+	end
+
+	def home_screen #the main controller for punchy
+		while @command != ":q"
+			self.display_menu
+
+			self.directory_listing
 			
-			if commandI <= (@directories.size) && commandI > 0
-				puts "You selected #{@directories[commandI]}"
-			end
+			self.input
 
 			@directories = Array.new
 		end
