e0622a0e20bc664f335818b8838c29c3e168f8ad
Zach McGonigal <zach.mcgonigal@gmail.com>
2012-04-15 00:43:44 -0400

    added project class. changed some instance variables to class variables. minor tweaks

--- punchlist.rb
+++ punchlist.rb
@@ -12,16 +12,19 @@ class Punchy
 
 	def initialize
 		@command = ""	
-		@dirs = Array.new 
-		@curDir = Dir.pwd
+		@@dirs = Array.new 
+		@@curDir = Dir.pwd
 	end
 
 	# Lists all the directories in the current path not starting with a "."
 	def dir_listing
 		Dir.glob('*') do |f|#go through all the files in the directory that don't start with a "."
 				if File.directory?(f)#if file is a directory, then it's a project
-					@dirs[@dirs.size + 1] = f#add the file to the directory arrray
-					puts "[#{@dirs.size - 1}] - #{f}"#display the directory and the number associated to it
+					if @@dirs.size == 0
+						@@dirs[0] = nil
+					end
+					@@dirs[@@dirs.size] = f#add the file to the directory arrray
+					puts "[#{@@dirs.size - 1}] - #{f}"#display the directory and the number associated to it
 				end
 		end
 	end
@@ -29,7 +32,7 @@ class Punchy
 	#the menu to be looped through
 	def display_menu
 		puts ""
-		puts "You are currently in #{@curDir}"
+		puts "You are currently in #{@@curDir}"
 		puts "Enter the number next to the project file to open that project, :q to quit"
 		puts ""
 	end
@@ -38,12 +41,11 @@ class Punchy
 	def input
 		@command = gets.to_s.strip #must strip to get rid of \n when user hits enter
 		commandI = @command.to_i #convert input to integer to see what number they pressed
-		
-		if commandI < (@dirs.size) && commandI > 0 #many strings will convert to integer 0, therefore we cannot use 0 as a number to be used
-			puts "You selected #{@dirs[commandI]}"
-			self.open_dir(@dirs[commandI])
+		if commandI < (@@dirs.size) && commandI > 0 #many strings will convert to integer 0, therefore we cannot use 0 as a number to be used
+			puts "You selected #{@@dirs[commandI]}"
+			self.open_dir(@@dirs[commandI])
 		elsif @command == ":q"
-			return #quite command entered
+			return #quit command entered
 		elsif
 			puts "Invalid selection. Try again"
 		end
@@ -51,12 +53,20 @@ class Punchy
 
 	#open the directory the user entered
 	def open_dir(dir_path)
-		Dir.chdir(@curDir + "/#{dir_path}") #enter directory selected by user
-		@curDir = Dir.pwd #update current directory
-		puts Dir.pwd #for now, echo out current directory. need to instantiate the new project class here!
-	end
+		Dir.chdir(@@curDir + "/#{dir_path}") #enter directory selected by user
+		@@curDir = Dir.pwd #update current directory
+		
+		project = Project.new
+		project.home_screen
 
-	def home_screen #the main controller for punchy
+		#we return back to here when project class returns by calling return in the input function
+		@@dirs = Array.new
+		Dir.chdir('..')
+		@@curDir = Dir.pwd
+	end
+	
+	#the main controller for punchy
+	def home_screen 
 		while @command != ":q"
 			self.display_menu
 
@@ -64,9 +74,27 @@ class Punchy
 			
 			self.input
 
-			@dirs = Array.new
+			@@dirs = Array.new
 		end
 	end
+end
+
+class Project < Punchy
+	
+	#reset the listing of dirs, command is an instance variable not a class variable because it should not be shared
+	def initialize
+		@@dirs = Array.new
+		@command = ""
+	end
+
+	#slightly different menu layout for when inside a project folder
+	def display_menu
+		puts ""
+		puts "You are currently in #{@@curDir}"
+		puts "Enter the number next to the category, :q to back to your punchlist"
+		puts ""
+	end
+
 end
 
 punchy = Punchy.new
